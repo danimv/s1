e80b608ae2b4a125d564376100e8a538d90b049c
danimv <danimv87@gmail.com>
2023-12-17 22:53:45 +0100

    mongodb_cloud

--- app.js
+++ app.js
@@ -12,51 +12,56 @@ const flash = require('connect-flash');
 const User = require('./models/user');
 const path = require('path');
 require('dotenv').config();
+const uri = "mongodb+srv://danimv87:dmv879731@foodup.xfpdje1.mongodb.net/?retryWrites=true&w=majority";
 
-// const initDB = require('./initDB');
-// initDB();
 
-//Using Dependencies
+const initDB = require('./initDB');
+initDB();
+
 // mongoose.connect('mongodb://127.0.0.1:27017/foodup', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-// });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
-app.use(cors({
-  origin: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 
-
 app.use(express.static(__dirname + '/public'));
 // app.use(express.static(path.join(__dirname, 'gr8/public')));
 app.use(methodOverride('_method'));
-// app.use(
-//   expressSession({
-//     secret: 'slfsjfhkdshgkfdsh',
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
+app.use(
+  expressSession({
+    secret: 'slfsjfhkdshgkfdsh',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(flash());
 app.use(cookieParser());
 
 //Initializing Passport//
 app.use(passport.initialize());
-app.use(passport.session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }, // Set secure to true in production with HTTPS
-}));
+app.use(
+  passport.session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set secure to true in production with HTTPS
+  })
+);
 
 passport.use(new LocalStrategy(User.authenticate()));
 
