298f03a0f58f30f6de7c923ea9a3a13abf047e1b
danimv <danimv87@gmail.com>
2023-12-20 03:27:07 +0100

    social_contrib

---
+++ models/contribModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const contribDataSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  image: String,
+  lloc: String,
+  tipus: String,
+  quantitat: String,
+  unitat: String,
+  imageName: String,
+  imagePath: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    username: String,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+});
+
+const contribModel = mongoose.model('contrib', contribDataSchema);
+
+module.exports = contribModel;
