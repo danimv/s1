09940317206a6f73af1a37ba1b7a927c77ee9de5
danimv <danimv87@gmail.com>
2023-12-20 02:42:14 +0100

    social_contrib

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const Stripe = require('stripe');
+const initDB = require('initDB');
+
 
 const app = express();
 app.use(cors());
